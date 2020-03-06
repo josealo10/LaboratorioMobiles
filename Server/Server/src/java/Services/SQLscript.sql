0aa0b42f7d1ab510d4b1cbd4bb6c6261bae05e75
Unknown <josealonso.alfa@gmail.com>
2020-03-06 11:13:47 -0600

    Creacion app web

--- Server/Server/src/java/Services/SQLscript.sql
+++ Server/Server/src/java/Services/SQLscript.sql
@@ -1,23 +1,24 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  jaalf
- * Created: 04/03/2020
- */
  create table if not exists carrera(
 	codigo int primary key not null auto_increment,
     nombre varchar(30)
+    /*lista de cursos */
+ );
+ 
+ create table if not exists curso(
+	codigo int primary key not null auto_increment,
+    nombre varchar(30),
+	creditos int,
+    horas_semanales int
  );
  
  
  create table if not exists alumno(
-	cedula int primary key not null auto_increment,
+	cedula int not null auto_increment,
     nombre varchar(30),
     telefono int,
     email varchar(30),
-    carrera int foreign key carrera(codigo)    
+	carrera int,
+    primary key(cedula),
+    foreign key (carrera) references carrera(codigo)    
  );
 

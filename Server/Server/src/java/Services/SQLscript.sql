1ef5bbe1ff6da4f8c26514b73ba5fcd05a5a5c83
Unknown <josealonso.alfa@gmail.com>
2020-04-08 23:14:33 -0600

    api con cursos matriculados y login devuelve cedula

--- Server/Server/src/java/Services/SQLscript.sql
+++ Server/Server/src/java/Services/SQLscript.sql
@@ -26,11 +26,19 @@
     nombre varchar(30),
     telefono int,
     email varchar(30),
-	carrera int,
-    usuario varchar(30),
+	carrera int not null,
+    usuario varchar(30) not null,
     primary key(cedula),
     foreign key (carrera) references carrera(codigo),
     foreign key (usuario) references usuario(username)
+ );
+ 
+ create table if not exists mobiles.cursosMatriculados(
+	alumno int not null,
+    curso int not null,
+    primary key (alumno,curso),
+    foreign key (alumno) references alumno(cedula),
+    foreign key (curso) references curso(codigo)
  );
  
  DELIMITER $$
@@ -39,6 +47,13 @@
 	select username, clave, permiso from mobiles.usuario 
 		where usuario.username = username
 			and usuario.clave = clave;
+ end
+ $$
+ 
+  DELIMITER $$
+ create procedure mobiles.insertUsuario(username varchar(30 ), clave varchar(30),permiso varchar(30))
+ begin
+	insert into mobiles.usuario(username,clave,permiso) values(username,clave,permiso);
  end
  $$
  
@@ -94,11 +109,44 @@ DELIMITER $$
  end
  $$
 
- DELIMITER $$
- create procedure mobiles.insertAlumno(cedula int, nombre varchar(30 ), telefono int, email varchar(30) , carrera int)
+  DELIMITER $$
+ create procedure mobiles.getAlumno(cedula int)
  begin
-	insert into mobiles.alumno(cedula, nombre, telefono , email, carrera) 
-    values(cedula, nombre, telefono , email, carrera);
+	select * from mobiles.alumno
+    where mobiles.alumno.cedula = cedula;
+ end
+ $$
+ 
+  DELIMITER $$
+ create procedure mobiles.getAlumnoWithUsername(username varchar(30))
+ begin
+	select mobiles.alumno.cedula from mobiles.alumno, mobiles.usuario
+    where mobiles.alumno.usuario = mobiles.usuario.username
+    and mobiles.usuario.username = username;
+ end
+ $$
+ 
+  DELIMITER $$
+ create procedure mobiles.getCursosMatriculados(cedula int)
+ begin
+	select mobiles.curso.codigo,
+    mobiles.curso.nombre,
+    mobiles.curso.horas_semanales,
+    mobiles.curso.creditos,
+    mobiles.carrera.nombre 
+		from mobiles.alumno, mobiles.curso,mobiles.cursosmatriculados,mobiles.carrera
+			where mobiles.alumno.cedula = mobiles.cursosmatriculados.alumno
+			and mobiles.cursosmatriculados.curso = mobiles.curso.codigo
+			and mobiles.curso.carrera = mobiles.carrera.codigo
+				and mobiles.alumno.cedula = cedula;
+ end
+ $$
+
+ DELIMITER $$
+ create procedure mobiles.insertAlumno(cedula int, nombre varchar(30 ), telefono int, email varchar(30) , carrera int, usuario varchar(30))
+ begin
+	insert into mobiles.alumno(cedula, nombre, telefono , email, carrera, usuario) 
+    values(cedula, nombre, telefono , email, carrera, usuario);
  end
  $$
  
@@ -131,6 +179,7 @@ DELIMITER $$
  $$
  
  insert into mobiles.usuario(username,clave,permiso) values('111','111','Alumno');
+ insert into mobiles.usuario(username,clave,permiso) values('333','333','Alumno');
  insert into mobiles.usuario(username,clave,permiso) values('222','222','Administrador');
  
  insert into mobiles.carrera(nombre) values('Informatica');
@@ -138,37 +187,60 @@ DELIMITER $$
  
  insert into mobiles.curso(nombre, creditos , horas_semanales, carrera) values('Plataformas mobiles',4,4,1);
  insert into mobiles.curso(nombre, creditos , horas_semanales, carrera) values('Inge 3',4,4,1);
+ insert into mobiles.curso(nombre, creditos , horas_semanales, carrera) values('Funda',4,4,1);
+ insert into mobiles.curso(nombre, creditos , horas_semanales, carrera) values('Progra 1',4,4,1);
+ insert into mobiles.curso(nombre, creditos , horas_semanales, carrera) values('Redes',4,4,1);
+ 
  insert into mobiles.curso(nombre, creditos , horas_semanales, carrera) values('Finanzas',4,4,2);
  insert into mobiles.curso(nombre, creditos , horas_semanales, carrera) values('Contabilidad',4,4,2);
  
- insert into mobiles.alumno(cedula,nombre,telefono,email,carrera) values(402420953,'Alonso',89620145,'alonso@gmail.com',1);
+ insert into mobiles.alumno(cedula,nombre,telefono,email,carrera,usuario) values(402420953,'Alonso', 89620145,'alonso@gmail.com',1,111);
+ insert into mobiles.alumno(cedula,nombre,telefono,email,carrera,usuario) values(171717,'Fazio', 89620145,'fazio@gmail.com',1,333);
+ 
+ insert into mobiles.cursosMatriculados(alumno,curso) values(402420953,1);
+ insert into mobiles.cursosMatriculados(alumno,curso) values(402420953,2);
+ insert into mobiles.cursosMatriculados(alumno,curso) values(402420953,3);
+ 
+ insert into mobiles.cursosMatriculados(alumno,curso) values(171717,3);
+ insert into mobiles.cursosMatriculados(alumno,curso) values(171717,4);
+ insert into mobiles.cursosMatriculados(alumno,curso) values(171717,5);
 
  call insertCurso('Soporte', '4', '4','1')
- call mobiles.getUsuario('111','111');
+ call mobiles.getUsuario('101','111');
+ call mobiles.insertUsuario('444','444','Alumno');
  call mobiles.getCursos();
  call mobiles.getCarreras();
  call mobiles.getCarrera(1);
  call mobiles.getAlumnos();
+ call mobiles.getAlumno(402420953);
+ call mobiles.getAlumnoWithUsername('111');
+ call mobiles.getCursosMatriculados(402420953);
  call mobiles.deleteCurso(11);
  call mobiles.updateCurso(4, 'Funda', 4, 5, 1);
- call mobiles.insertAlumno(402420, 'Alonso', '89620145', 'josealonso@gmail.com','1');
+ call mobiles.insertAlumno(402420, 'Alonso', '89620145', 'josealonso@gmail.com',1,111);
  call mobiles.updateAlumno(40256420, 'pedro', 89620145, 'josealonso@gmail.com',1);
  call mobiles.deleteAlumno(402420);
  
+ 
+ drop table mobiles.cursosMatriculados;
  drop table mobiles.alumno;
  drop table mobiles.curso;
  drop table mobiles.carrera;
  drop table mobiles.usuario;
  
  drop procedure mobiles.getUsuario;
+ drop procedure mobiles.insertUsuario;
  
  drop procedure mobiles.getCarrera;
  drop procedure mobiles.getCarreras;
  
  drop procedure mobiles.getAlumnos;
+ drop procedure mobiles.getAlumno;
+ drop procedure mobiles.getAlumnoWithUsername;
  drop procedure mobiles.insertAlumno;
  drop procedure mobiles.deleteAlumno;
  drop procedure mobiles.updateAlumno;
+ drop procedure mobiles.getCursosMatriculados;
  
  drop procedure mobiles.getCursos;
  drop procedure mobiles.insertCurso;

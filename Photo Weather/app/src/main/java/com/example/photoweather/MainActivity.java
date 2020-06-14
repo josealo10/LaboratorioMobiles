9da89c8d384a3918e44a6b5381b311dbc5b2b519
Unknown <josealonso.alfa@gmail.com>
2020-06-14 03:06:11 -0600

    Sensor de luz y algo de blue tooth

--- Photo Weather/app/src/main/java/com/example/photoweather/MainActivity.java
+++ Photo Weather/app/src/main/java/com/example/photoweather/MainActivity.java
@@ -1,40 +1,84 @@
-package com.example.photoweather;
+package com.example.photo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import com.example.photo.databinding.ActivityMainBinding;
 
-import com.example.photoweather.databinding.ActivityMainBinding;
+import java.util.UUID;
 
 import es.dmoral.toasty.Toasty;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static final UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+
 
     private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
     private ActivityMainBinding binding;
     private Uri imageURI;
+
+    private SensorManager sensorManager;
+    private Sensor sensorLuz;
+    private float valorLuz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorLuz = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        binding.tvEnviarBluetooth.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Intent listBluetoothIntent = new Intent(this, ListBluetoothActivity.class);
+                //startActivity(listBluetoothIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorLuz, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
     public void onAbrirCamaraClick(View view) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+
+            if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED) {
+
                 String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
                 requestPermissions(permissions, PERMISSION_CODE);
 
@@ -52,7 +96,12 @@ public class MainActivity extends AppCompatActivity {
         contentValues.put(MediaStore.Images.Media.TITLE, "Imagen 1");
         contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Tomada con el celular");
         imageURI = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
-        startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE).putExtra(MediaStore.EXTRA_OUTPUT, imageURI), 1);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageURI);
+        startActivityForResult(cameraIntent, 1);
+
+        //startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE).putExtra(MediaStore.EXTRA_OUTPUT, imageURI), 1);
     }
 
     @Override
@@ -76,6 +125,17 @@ public class MainActivity extends AppCompatActivity {
 
         if (resultCode == RESULT_OK && requestCode == 1) {
             binding.ivPhoto.setImageURI(imageURI);
+            binding.tvNivelLuz.setText(valorLuz + "");
         }
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float luz = sensorEvent.values[0];
+        valorLuz = luz;
+        Log.d("Nivel luz: ", luz+"");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {}
 }
